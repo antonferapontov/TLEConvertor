a6da2d814168f6284b8d56a868936d3ff095d4de
antonferapontov <ferapontov00@inbox.ru>
2016-02-23 13:31:30 +0400

    Added files via upload

---
+++ TLEConvertor/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(533, 62);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(551, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 91);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(157, 110);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 617);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 4);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(309, 617);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(337, 4);
+            this.listBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 110);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Скопировать в буфер";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 65);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "1. Введите TLE.\r\n2. Нажмите кнопку \"Запуск\"\r\n3. Если вы хотите скопировать резуль" +
+                "тат в буфер \r\nнажмите \"Скопировать в буфер\"\r\n4. Вы можите взять TLE на этом сайт" +
+                "е:\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(296, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "TLEConventor 1.0 сборка: 2016-01-17 (c) Anton Ferapontov";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(648, 2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(10, 10);
+            this.textBox3.TabIndex = 11;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(492, 143);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(144, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.space-track.org";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 206);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "TLEConventor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ TLEConvertor/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace("   "," ");
+            textBox1.Text = textBox1.Text.Replace("  "," ");
+            textBox1.Text = textBox1.Text.Replace("  "," ");
+            textBox1.Text = textBox1.Text.Replace("-", "");
+            string[] spilts = textBox1.Text.Split(new Char[] { ' ','\t' });
+            int i = spilts.Length;
+            spilts[2] = spilts[2].Remove(2);
+            spilts[1] = spilts[1].Remove(5);
+            spilts[10] = spilts[10].Replace(".", ",");
+            double orbit = Double.Parse(spilts[10].Replace(".",","));
+            int year = Int32.Parse(spilts[2]);
+            if(year < 57)
+            {
+                year += 2000;
+            }
+            else
+            {
+                year += 1900;
+            }
+            //spilts[15] = spilts[15].Remove(11);
+            textBox3.Text = spilts[15];
+            spilts[12] = ("0." + spilts[12]);
+            textBox2.Text = (" "+i);
+            for (int x = 0; x < spilts.Length; x++)
+            {
+               listBox1.Items.Add(spilts[x]+"("+x+")");
+            }
+            
+            string s = spilts[15];
+            string s1 = spilts[12];
+            s.Replace('.',',');
+            s1.Replace('.', ',');
+            double Revday = double.Parse(s.Replace('.', ','));
+            double Ecc = double.Parse(s1.Replace('.', ','));
+       
+            double apogee = (0.0743668 / Revday);
+            
+            apogee = apogee * 229.18311;
+           
+            apogee = Math.Pow(apogee, 2.0);
+            
+            apogee = Math.Pow(apogee, (1/3.0));
+           
+            apogee = apogee * (1 + Ecc);
+           
+            apogee = apogee - 1;
+           
+            apogee = apogee * 6378.15;
+           
+
+         
+            double perigee = (0.0743668 / Revday);
+          
+            perigee = perigee * 229.18311;
+           
+            perigee = Math.Pow(perigee, 2.0);
+          
+            perigee = Math.Pow(perigee, (1 / 3.0));
+           
+            perigee = perigee * (1 - Ecc);
+           
+            perigee = perigee - 1;
+        
+            perigee = perigee * 6378.15;
+            
+
+            
+            textBox2.Text = (
+                "Параметры орбиты:      "+
+                "Апогей:" + Math.Round(apogee)+" км               "+
+                "Перигей:" + Math.Round(perigee)+" км                    "+
+                "Наклонение: "+Math.Round(orbit)+"°                      "+
+               "Год запуска: "+ year+"              "+
+               "Номер объекта (в каталоге NORAD): "+spilts[1]);
+            
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox2.Text);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
